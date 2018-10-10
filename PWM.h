c508f7da902f0ee1e924e2ce62b8f1c373774802
Robert Leonard <bl.cirrus@gmail.com>
2018-10-10 13:16:48 -0400

    Added object prototypes

--- PWM.h
+++ PWM.h
@@ -5,4 +5,15 @@
 #ifndef _PWM_LIB_
 #define _PWM_LIB_
 
+#include <stdint.h>
+
+struct PWM {
+
+};
+
+void PWM_init(PWM *pwm);
+void PWM_set(PWM *pwm, uint16_t counter);
+void PWM_start(PWM *pwm);
+void PWM_stop(PWM *pwm);
+
 #endif
--- sensors/INA219.h
+++ sensors/INA219.h
@@ -8,5 +8,18 @@
 
 #include <stdint.h>
 
+#define INA219_ADDRESS  (0x40)
+
+// todo: define all the register addresses for the INA219...
+
+struct INA219 {
+    uint8_t addr = INA219_ADDRESS;
+};
+
+void INA219_init(INA219 *ina);
+int16_t INA219_getBusVoltage(INA219 *ina);
+int16_t INA219_getShuntVoltage(INA219 *ina);
+int16_t INA219_getCurrent(INA219 *ina);
+int16_t INA219_getPower(INA219 *ina);
 
 #endif
