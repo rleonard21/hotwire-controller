0ff79488a9a3d02f36d899ba143d7d545e0f89ad
Robert Leonard <bl.cirrus@gmail.com>
2018-10-11 00:30:16 -0400

    Added correct BSD copyright header

--- sensors/INA219.c
+++ sensors/INA219.c
@@ -1,6 +1,16 @@
-// INA219.cpp
-// Hotwire Controller Project
-// Adapted from Adafruit INA219 C++ Library
-// Written by Robert Leonard
-
+/*!
+ * This is a library for the Adafruit INA219 breakout board
+ * ----> https://www.adafruit.com/products/904
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Written by Kevin "KTOWN" Townsend for Adafruit Industries.
+ *
+ * BSD license, all text here must be included in any redistribution.
+ *
+ * Ported to C for use with standard AVR by Robert Leonard
+ * Hotwire Controller Project
+ */
 #include "INA219.h"
