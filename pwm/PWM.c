a8973e4dd51ee5ad6d3f0e3efd8f2ba6bbe99c0d
rleonard21 <bl.cirrus@gmail.com>
2018-10-12 16:49:23 -0400

    Added getter/setter/status functions

--- pwm/PWM.c
+++ pwm/PWM.c
@@ -7,9 +7,31 @@
 
 void PWM_init(struct PWM_16_bit *pwm) {
     pwm->counter = 0;
+    pwm->is_running = false;
+
+    // set the MOSFET pin an an output
+    // enable the 16 bit timer
+    // set the timer prescaler
 }
 
+void PWM_set(struct PWM_16_bit *pwm, uint16_t setting) {
+    pwm->counter = setting;
+}
+
+uint16_t PWM_get(struct PWM_16_bit *pwm) {
+    return pwm->counter;
+}
+
+bool PWM_is_running(struct PWM_16_bit *pwm) {
+    return pwm->is_running;
+}
 
 void PWM_start(struct PWM_16_bit *pwm) {
+    pwm->is_running = true;
+    // set the timer register to pwm->counter
+}
 
+void PWM_stop(struct PWM_16_bit *pwm) {
+    pwm->is_running = false;
+    // set the timer register to zero
 }
--- pwm/PWM.h
+++ pwm/PWM.h
@@ -9,10 +9,16 @@
 
 struct PWM_16_bit {
     uint16_t counter;
+    bool is_running;
 };
 
 void PWM_init(struct PWM_16_bit *pwm);
-void PWM_set(struct PWM_16_bit *pwm, uint16_t counter);
+
+void PWM_set(struct PWM_16_bit *pwm, uint16_t setting);
+uint16_t PWM_get(struct PWM_16_bit *pwm);
+
+bool PWM_is_running(struct PWM_16_bit *pwm);
+
 void PWM_start(struct PWM_16_bit *pwm);
 void PWM_stop(struct PWM_16_bit *pwm);
 
