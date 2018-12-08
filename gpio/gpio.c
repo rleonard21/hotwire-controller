a85eb807a688accd912c0b9ad820bbe979f0c89c
Robert Leonard <bl.cirrus@gmail.com>
2018-12-08 16:29:46 -0500

    Added implementations and RMEs

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -1,20 +1,70 @@
 #include "gpio.h"
 
-void GPIO_blink(uint8_t *port, uint8_t position) {
-    while(1) {
-        *port ^= _BV(position);
-        _delay_ms(100);
-    }
+// MODIFIES: Modifies the underlying DDRx port.
+// EFFECTS:  Sets the position of the DDRx port to input or output according
+// 			 to page 101 of the 328P datasheet.
+void GPIO_init(volatile uint8_t *ddr, volatile uint8_t *port, uint8_t position, uint8_t type) {
+	switch(type) {
+		case OUTPUT_SINK:
+			*ddr |= _BV(position);
+			*port &= ~_BV(position);
+			break;
+
+		case OUTPUT_SOURCE:
+			*ddr |= _BV(position);
+			*port |= _BV(position);
+			break;
+
+		case INPUT:
+			*ddr &= ~_BV(position);
+			*port |= _BV(position);
+			// need to set PUD to zero.
+			break;
+
+		case INPUT_PULLUP:
+			*ddr &= ~_BV(position);
+			*port |= _BV(position);
+			// need to set PUD to one.
+			break;
+
+			default:
+			break;
+	}
 }
 
-void GPIO_toggle(uint8_t *port, uint8_t position) {
-    *port ^= _BV(position);
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Generates an infinite blink signal on the GPIO.
+void GPIO_blink(volatile uint8_t *port, uint8_t position, uint8_t interval) {
+	while (1) {
+		*port ^= _BV(position);
+		delay(interval);
+	}
 }
 
-void GPIO_on(uint8_t *port, uint8_t position) {
-    *port |= _BV(position);
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Toggles the position bit on PORTx.
+void GPIO_toggle(volatile uint8_t *port, uint8_t position) {
+	*port ^= _BV(position);
 }
 
-void GPIO_off(uint8_t *port, uint8_t position) {
-    *port &= ~_BV(position);
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Writes a 1 at the position bit on PORTx.
+void GPIO_on(volatile uint8_t *port, uint8_t position) {
+	*port |= _BV(position);
+}
+
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Writes a 0 at the position bit on PORTx.
+void GPIO_off(volatile uint8_t *port, uint8_t position) {
+	*port &= ~_BV(position);
+}
+
+
+// EFFECTS:  A simple obstructive delay implementation.
+static void delay(uint16_t interval) {
+	while(interval--) _delay_ms(1);
 }
--- gpio/gpio.h
+++ gpio/gpio.h
@@ -13,9 +13,38 @@
 #include <util/delay.h>
 #include <stdint.h>
 
-void GPIO_toggle(uint8_t *port, uint8_t position);
-void GPIO_blink(uint8_t *port, uint8_t position);
-void GPIO_on(uint8_t *port, uint8_t position);
-void GPIO_off(uint8_t *port, uint8_t position);
+#define OUTPUT_SINK 	0
+#define OUTPUT_SOURCE 	1
+#define	INPUT			2
+#define INPUT_PULLUP 	3
+
+// MODIFIES: Modifies the underlying DDRx port.
+// EFFECTS:  Sets the position of the DDRx port to input or output according
+// 			 to page 101 of the 328P datasheet.
+void GPIO_init(volatile uint8_t *ddr, volatile uint8_t *port, uint8_t position, uint8_t type);
+
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Generates an infinite blink signal on the GPIO.
+void GPIO_blink(volatile uint8_t *port, uint8_t position, uint8_t interval);
+
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Toggles the position bit on PORTx.
+void GPIO_toggle(volatile uint8_t *port, uint8_t position);
+
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Writes a 1 at the position bit on PORTx.
+void GPIO_on(volatile uint8_t *port, uint8_t position);
+
+
+// MODIFIES: Modifies the underlying PORTx.
+// EFFECTS:  Writes a 0 at the position bit on PORTx.
+void GPIO_off(volatile uint8_t *port, uint8_t position);
+
+
+// EFFECTS:  A simple obstructive delay implementation.
+static void delay(uint16_t interval);
 
 #endif
