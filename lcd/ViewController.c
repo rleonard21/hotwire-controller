e266495a274ade76e1381dcf9729b78f48b0b468
rleonard21 <bl.cirrus@gmail.com>
2019-07-06 01:56:48 -0500

    Added cursor struct

--- lcd/ViewController.c
+++ lcd/ViewController.c
@@ -7,23 +7,77 @@
 #include "lcd.h"
 #include "StringUtility.h"
 
+#include <string.h>
+#include <stdio.h>
+
 // EFFECTS: these two arrays will represent lines 1 and 2 of the LCD
-static char line1[] = "................\n";
-static char line2[] = "................\n";
+static char current_line_1[] = "................\n";
+static char current_line_2[] = "................\n";
+static char next_line_1[] = "................\n";
+static char next_line_2[] = "................\n";
 
 // EFFECTS: updates the LCD to display lines 1 and 2
 static void update_lcd() {
-	lcd_puts(line1);
-	lcd_puts(line2);
+	if (strcmp(current_line_1, next_line_1) ||
+	    strcmp(current_line_2, next_line_2)) {
+
+		strcpy(current_line_1, next_line_1);
+		strcpy(current_line_2, next_line_2);
+
+		lcd_puts(current_line_1);
+		lcd_puts(current_line_2);
+	}
 }
 
 // EFFECTS: displays the startup page
 void VC_startup_screen() {
+	lcd_gotoxy(0, 0);
+
 	lcd_puts("hotwire v1.1\n");
 	lcd_puts("\n");
 }
 
+// EFFECTS: displays the main menu page
+void VC_main_menu(struct Cursor cursor, uint8_t power, uint8_t voltage) {
+	lcd_gotoxy(0, 0);
+
+//	strcpy(next_line_1, "< READY   12.04v\n");
+//	strcpy(next_line_2, "> Duty:      73%\n");
+
+	sprintf(next_line_1, "< READY   12.04v\n");
+	sprintf(next_line_2, "> Duty:     %d\n", OCR1B);
+
+	update_lcd();
+	lcd_gotoxy(cursor.x, cursor.y);
+}
+
+// EFFECTS: displays the LCD settings page
+void VC_settings_lcd(struct Cursor cursor) {
+	lcd_gotoxy(0, 0);
+
+	sprintf(next_line_1, "< Bright:   >%d\n", OCR0B);
+	sprintf(next_line_2, "  Contrast: >%d\n", OCR0A);
+
+	update_lcd();
+	lcd_gotoxy(cursor.x, cursor.y);
+}
+
 // EFFECTS: prints an error message
 void VC_page_incomplete() {
-	lcd_puts("Page incomplete.\n\n");
+	lcd_gotoxy(0, 0);
+
+	strcpy(next_line_1, "Page incomplete.\n");
+	strcpy(next_line_2, "...............1\n");
+
+	update_lcd();
+}
+
+// EFFECTS: prints an error message
+void VC_page_incomplete_2() {
+	lcd_gotoxy(0, 0);
+
+	strcpy(next_line_1, "Page incomplete.\n");
+	strcpy(next_line_2, "...............2\n");
+
+	update_lcd();
 }
--- lcd/ViewController.h
+++ lcd/ViewController.h
@@ -7,10 +7,24 @@
 
 #include <stdint.h>
 
+struct Cursor {
+	uint8_t x;
+	uint8_t y;
+};
+
 // EFFECTS: displays the startup page
 void VC_startup_screen();
 
+// EFFECTS: displays the main menu page
+void VC_main_menu(struct Cursor cursor, uint8_t power, uint8_t voltage);
+
+// EFFECTS: displays the LCD settings page
+void VC_settings_lcd(struct Cursor cursor);
+
 // EFFECTS: prints an error message
 void VC_page_incomplete();
+
+// EFFECTS: prints an error message
+void VC_page_incomplete_2();
 
 #endif //HOTWIRE_CONTROLLER_V1_1_VIEWCONTROLLER_H
