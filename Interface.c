17b128797bc54026d97b3f524624cc69d2b088c1
rleonard21 <bl.cirrus@gmail.com>
2019-07-06 02:26:16 -0500

    Added controllers for LCD settings and main

--- Interface.c
+++ Interface.c
@@ -2,51 +2,141 @@
 // Created by Robert Leonard on 2019-07-04.
 //
 
+#include <stddef.h>
 #include "Interface.h"
 
 #include "lcd/ViewController.h"
 #include "encoder/Encoder.h"
 #include "hotwire/Hotwire.h"
+#include "feedback/Buzzer.h"
+#include "lcd/LCDControl.h"
 
-#define SWITCH_HOLD_TIME 100
+#include "lcd/lcd.h"
+
+#define SWITCH_HOLD_TIME 200000
 
 // EFFECTS: handles the main menu
-uint8_t Interface_main() {
+uint8_t Interface_main_hotwire_off(uint8_t next_view) {
 	/* TODO:
 	 *  * display the status and prompt (START/STOP)
 	 *  * display the voltage
 	 *  * display the percent
+	 *  * handle the cursor / rotation
 	 *  * ~handle the hotwire start/stop
 	 *  * ~handle the transition to the settings page
 	 */
 
-	// Stop the running hotwire on button press
-	if(Encoder_switch_is_pressed() && !Hotwire_is_running()) {
-		Hotwire_stop();
-		return VIEW_MAIN;
-	}
+	// Display the main menu
+	// TODO: remain (1,1) with voltage and duty cycle...
 
-	// Start the paused hotwire on button hold
-	if(Encoder_switch_is_pressed() && !Hotwire_is_running()) {
-		uint16_t timer = 0;
-		while(Encoder_switch_is_high()) timer++;
+	struct Cursor cursor = {0, 0};
 
-		if(timer > SWITCH_HOLD_TIME) {
-			Hotwire_start();
-		} else {
-			return VIEW_SETTINGS_MAIN;
+	Hotwire_set(100);
+
+	lcd_command(LCD_DISP_ON_BLINK);
+	VC_main_menu(cursor, 1, 1);
+
+	while (1) {
+		if (Encoder_rotary_read() == ROTATE_RIGHT) {
+			cursor.y = 1;
+			lcd_gotoxy(cursor.x, cursor.y);
+		}
+
+		if (Encoder_rotary_read() == ROTATE_LEFT) {
+			cursor.y = 0;
+			lcd_gotoxy(cursor.x, cursor.y);
+		}
+
+		if (Encoder_switch_is_pressed()) {
+			if (Encoder_switch_is_held(SWITCH_HOLD_TIME)) {
+				Hotwire_start();
+				Buzzer_play(1, 50);
+				return VIEW_SETTINGS_LCD;
+			}
+
+			if (cursor.y) {
+				while (!Encoder_switch_is_high()) {
+					if (Encoder_rotary_read() == ROTATE_RIGHT) {
+						Hotwire_set_increment();
+						VC_main_menu(cursor, 1, 1);
+					}
+
+					if (Encoder_rotary_read() == ROTATE_LEFT) {
+						Hotwire_set_decrement();
+						VC_main_menu(cursor, 1, 1);
+					}
+				}
+			} else {
+				return VIEW_SETTINGS_MAIN;
+			}
 		}
 	}
-
-	return VIEW_MAIN;
 }
 
 // EFFECTS: handles the main settings page
-uint8_t Interface_settings_main() {
+uint8_t Interface_settings_main(uint8_t next_view) {
 	VC_page_incomplete();
+
+	if (Encoder_switch_is_pressed()) {
+		return VIEW_MAIN_STOPPED;
+	}
+
+	return VIEW_SETTINGS_MAIN;
 }
 
 // EFFECTS: handles the LCD settings page
-uint8_t Interface_settings_lcd() {
-	VC_page_incomplete();
+uint8_t Interface_settings_lcd(uint8_t next_view) {
+	struct Cursor cursor = {0, 0};
+
+	VC_settings_lcd(cursor);
+
+	while(1) {
+		// Handle moving the LCD selection cursor
+		if(Encoder_rotary_read() == ROTATE_LEFT) {
+			if(cursor.x && cursor.y)
+				cursor.y--;
+			else if(cursor.x == 12 && cursor.y == 0)
+				cursor.x -= 12;
+
+			lcd_gotoxy(cursor.x, cursor.y);
+			while(Encoder_rotary_read() != ROTATE_NONE);
+
+		} else if(Encoder_rotary_read() == ROTATE_RIGHT) {
+			if(!cursor.x && !cursor.y)
+				cursor.x += 12;
+			else if(cursor.x && !cursor.y)
+				cursor.y++;
+
+			lcd_gotoxy(cursor.x, cursor.y);
+			while(Encoder_rotary_read() != ROTATE_NONE);
+		}
+
+		// Handle the encoder button press
+		if(Encoder_switch_is_pressed()) {
+			// Back button pressed
+			if(cursor.x == 0 && cursor.y == 0) {
+				return VIEW_SETTINGS_MAIN;
+			}
+
+			// Wait for encoder to be released
+			while(!Encoder_switch_is_released());
+
+			// Edit the LCD values until the encoder button is pressed
+			while(!Encoder_switch_is_high()) {
+				if (Encoder_rotary_read() == ROTATE_RIGHT) {
+					if(cursor.y) LCDControl_increment_contrast();
+					else LCDControl_increment_backlight();
+
+					VC_settings_lcd(cursor);
+				}
+
+				if (Encoder_rotary_read() == ROTATE_LEFT) {
+					if(cursor.y) LCDControl_decrement_contrast();
+					else LCDControl_decrement_backlight();
+
+					VC_settings_lcd(cursor);
+				}
+			}
+		}
+	}
 }
--- Interface.h
+++ Interface.h
@@ -7,18 +7,22 @@
 
 #include <stdint.h>
 
-#define VIEW_MAIN           0
-#define VIEW_SETTINGS_MAIN  1
-#define VIEW_SETTINGS_LCD   2
+#define VIEW_MAIN_STOPPED   0
+#define VIEW_MAIN_RUNNING   1
+#define VIEW_SETTINGS_MAIN  2
+#define VIEW_SETTINGS_LCD   3
 
-// EFFECTS: handles the main menu
-uint8_t Interface_main();
+// EFFECTS: handles the main menu when the hotwire is off
+uint8_t Interface_main_hotwire_off(uint8_t next_view);
+
+// EFFECTS: handles the main menu when the hotwire is on
+uint8_t Interface_main_hotwire_on(uint8_t next_view);
 
 // EFFECTS: handles the main settings page
-uint8_t Interface_settings_main();
+uint8_t Interface_settings_main(uint8_t next_view);
 
 // EFFECTS: handles the LCD settings page
-uint8_t Interface_settings_lcd();
+uint8_t Interface_settings_lcd(uint8_t next_view);
 
 // TODO:
 //  * add other settings pages
