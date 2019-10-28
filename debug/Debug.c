5e46cf5c1270a66be8d92bbef0c6cad5da8326fd
Robert Leonard <bl.cirrus@gmail.com>
2019-10-28 15:11:59 -0400

    Added debug interface over I2C bus

---
+++ debug/Debug.c
@@ -0,0 +1,18 @@
+// Debug.c
+// Created by Robert Leonard on 10/28/19.
+//
+
+#include "Debug.h"
+#include "../i2c/i2cmaster.h"
+
+// EFFECTS: writes the value to the I2C line
+void debug_write(uint8_t reg, int16_t value) {
+	uint8_t msb = (uint8_t) (value >> 8);			// extract higher 8 bits
+	uint8_t lsb = (uint8_t) ((value << 8) >> 8);	// extract lower 8 bits
+
+	i2c_start(DEBUG_ADDR, I2C_WRITE);
+	i2c_write(reg);
+	i2c_write(msb);		// send MSB
+	i2c_write(lsb);		// send LSB
+	i2c_stop();
+}
---
+++ debug/Debug.h
@@ -0,0 +1,14 @@
+// Debug.h
+// Created by Robert Leonard on 10/28/19.
+//
+
+#ifndef HOTWIRE_CONTROLLER_V1_1_DEBUG_H
+#define HOTWIRE_CONTROLLER_V1_1_DEBUG_H
+
+#include <stdint.h>
+
+#define DEBUG_ADDR (0x7F)
+
+void debug_write(uint8_t reg, int16_t value);
+
+#endif //HOTWIRE_CONTROLLER_V1_1_DEBUG_H
